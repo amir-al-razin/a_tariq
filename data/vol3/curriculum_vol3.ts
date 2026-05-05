33c8f406f999a9e9721e110c0190a2c1abd52851
amir-al-razin <amiralmohyminrazin@gmail.com>
2026-05-05 08:21:32 +0600

    Add Chapter 3 lessons to Volume 3 curriculum"

--- data/vol3/curriculum_vol3.ts
+++ data/vol3/curriculum_vol3.ts
@@ -17,6 +17,14 @@ import { lesson06 as ch2lesson06 } from './ch2/lesson06';
 import { lesson07 as ch2lesson07 } from './ch2/lesson07';
 import { lesson08 as ch2lesson08 } from './ch2/lesson08';
 
+import { lesson01 as ch3lesson01 } from './ch3/lesson01';
+import { lesson02 as ch3lesson02 } from './ch3/lesson02';
+import { lesson03 as ch3lesson03 } from './ch3/lesson03';
+import { lesson04 as ch3lesson04 } from './ch3/lesson04';
+import { lesson05 as ch3lesson05 } from './ch3/lesson05';
+import { lesson06 as ch3lesson06 } from './ch3/lesson06';
+import { lesson07 as ch3lesson07 } from './ch3/lesson07';
+
 import { lesson01 as ch4lesson01 } from './ch4/lesson01';
 import { lesson02 as ch4lesson02 } from './ch4/lesson02';
 
@@ -58,6 +66,21 @@ export const CHAPTERS_VOL3: ChapterData[] = [
             ch2lesson06,
             ch2lesson07,
             ch2lesson08,
+        ],
+    },
+    {
+        id: 3,
+        titleAr: 'الباب الثالث',
+        titleEn: 'Chapter Three',
+        subtitle: 'Advanced grammar and linguistic structures',
+        lessons: [
+            ch3lesson01,
+            ch3lesson02,
+            ch3lesson03,
+            ch3lesson04,
+            ch3lesson05,
+            ch3lesson06,
+            ch3lesson07,
         ],
     },
 
